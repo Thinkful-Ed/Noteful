c259907f54afc793dad17f7a516b85180882d792
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-11-17 15:54:55 +0300

    Add show action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,6 +17,10 @@ class NotesController < ApplicationController
     end
   end
 
+  def show
+    @note = Note.find(params[:id])
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :content)
