94dc662d910b76e9daac27d87e2aefc71fd3d789
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-11-17 18:52:39 +0300

    Add author model to the app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110110008) do
+ActiveRecord::Schema.define(version: 20141117155214) do
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: true do |t|
     t.string   "title"
