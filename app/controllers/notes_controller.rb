84253a94070ca39610fd65ebe2fda743f5ba61d6
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-11-17 15:08:10 +0300

    Add create action with strong params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,5 +7,17 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def create
+    @note = Note.new(note_params)
+
+    @note.save
+    redirect to @note
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
   
 end
