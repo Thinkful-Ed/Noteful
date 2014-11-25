a6735f485c2d5a0b4eb8cd2dd954e64e7a11f100
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-11-25 04:14:26 +0300

    Link users and notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117174033) do
+ActiveRecord::Schema.define(version: 20141125011300) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141117174033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "author_id"
+    t.integer  "user_id"
   end
 
   add_index "notes", ["author_id"], name: "index_notes_on_author_id"
