b77d237bc7564450bfe465e95e38e52cc1e2bb63
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-11-25 04:17:13 +0300

    Link users and notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,7 +9,7 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.new(note_params)
 
     if @note.save
       redirect_to @note, notice: 'Success! You have just written a new note!'
